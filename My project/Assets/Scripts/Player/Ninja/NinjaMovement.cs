e331d2250a168f3d462470d2d054073c35edc439
PaulWeGeSch <116371757+PaulWeGeSch@users.noreply.github.com>
2023-05-25 11:05:37 +0200

    Animationen

--- My project/Assets/Scripts/Player/Ninja/NinjaMovement.cs
+++ My project/Assets/Scripts/Player/Ninja/NinjaMovement.cs
@@ -9,8 +9,13 @@ public class NinjaMovement : MonoBehaviour
 {
     public float MovementSpeed = 1;
     public float JumpForce = 1;
+    bool m_FacingRight = true;
+    float yDirection;
+
+    public Animator animator;
 
     public Collider2D groundCheck;
+    
 
     public Rigidbody2D rb;
     Vector2 moveDirection = Vector2.zero;
@@ -34,7 +39,7 @@ public class NinjaMovement : MonoBehaviour
 
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -42,24 +47,53 @@ public class NinjaMovement : MonoBehaviour
     {
         moveDirection = move.ReadValue<Vector2>();
         //jumpDirection = move.ReadValue
+        
+       
+        
+        if (moveDirection.x > 0.01)  // Frägt Geschwindigkeit ab, wenn sich Ninja bewegt startet die Run-Animation 
+        {
+            animator.SetBool("Run", true);
 
+            if (!m_FacingRight) Flip();
+        }
+        else
+        {
+            if (moveDirection.x < -0.01)
+            {
+                animator.SetBool("Run", true);
+
+                if (m_FacingRight) Flip();
+            }
+            else
+            {
+                animator.SetBool("Run", false);
+            }
+        }
         //Debug.Log(GroundCheckNinja.groundCheckNinja);
 
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveDirection.x * MovementSpeed, 0);
-        
 
         if (GroundCheckNinja.groundCheckNinja == true && Input.GetKeyDown("w"))
         {
-            Debug.Log(moveDirection.y);                                                     //moveDirection.y wird nicht ausgeslesen. Ist immer 0
-            rb.velocity = new Vector2(0, moveDirection.y * JumpForce);
+                                                              //moveDirection.y wird nicht ausgeslesen. Ist immer 0
+            rb.velocity = new Vector2(0, yDirection * JumpForce);                 // + 10 den Wert für Animationstest
 
             Debug.Log("springt");
         }
+        yDirection = moveDirection.y;
+        Debug.Log(moveDirection.y);
+    }
 
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
 
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
 }
